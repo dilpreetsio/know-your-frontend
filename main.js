b0ad62840a757bfdafbdf1585ba86e9fe1c7ce1e
Dilip <dilip.mari@gmail.com>
2017-10-14 09:39:18 +0530

    Added support for other front-end frameworks
    Added Knockbackjs

--- main.js
+++ main.js
@@ -2,14 +2,22 @@ var reactSelector = '[data-reactroot], [data-reactid]';
 var angularSelector = '[ng-app], [ng-model],[ng-controller],[ng-version],[ng-binding]';
 var emberSelector = '.ember-application';
 
+
 function checkFE() {
   var frontend = '';
+  var doc=document.all;
+  doc=[...doc];
+  var kb=doc.filter(function(item){ 
+    if(item.hasAttribute('kb-inject')) return item; 
+  });
   if (document.querySelector(reactSelector)) {
     frontend = 'react';
   } else if(document.querySelector(angularSelector)) {
     frontend = 'angular';
   } else if(document.querySelector(emberSelector)) {
     frontend = 'ember';
+  }else if(kb.length>0) {
+    frontend = 'KnockbackJs';
   } else {
     frontend = 'none';
   }
