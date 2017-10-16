122297c1e91831c342d789250ffc21035fb004a9
Vladimir Alabov <vladimir.alabov@bsc-ideas.com>
2017-10-16 12:49:16 +0300

    Added Vue.js and Knockout.js support

--- background.js
+++ background.js
@@ -8,6 +8,10 @@ chrome.runtime.onMessage.addListener(
       chrome.browserAction.setIcon({path: basePath + 'ember.png'});
     } else if (request.framework === 'angular') {
       chrome.browserAction.setIcon({path: basePath + 'angular.png'});
+    } else if (request.framework === 'vue') {
+      chrome.browserAction.setIcon({path: basePath + 'vue.png'});
+    } else if (request.framework === 'knockout') {
+      chrome.browserAction.setIcon({path: basePath + 'knockout.png'});
     } else if (request.framework === 'none') {
       chrome.browserAction.setIcon({path: basePath + 'unknown.png'});
     }
@@ -19,4 +23,4 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
 chrome.tabs.onActivated.addListener(function(tab) {
   chrome.tabs.sendMessage(tab.tabId, { status: true });
-})
+});
--- main.js
+++ main.js
@@ -1,19 +1,58 @@
 var reactSelector = '[data-reactroot], [data-reactid]';
 var angularSelector = '[ng-app], [ng-model],[ng-controller],[ng-version],[ng-binding]';
 var emberSelector = '.ember-application';
+var vueSelector = '#detect-vue';
+var knockoutSelector = '#detect-knockout';
+
+function injectScript() {
+  var script = document.createElement('script');
+  script.textContent = '(' +
+    function () {
+      var selector;
+      if (window.Vue && window.Vue.version) {
+          selector = "detect-vue";
+      }
+      if (window.ko && window.ko.version) {
+        selector = "detect-knockout";
+      }
+      if (selector) {
+        var el = document.createElement("span");
+        el.setAttribute("id", selector);
+        document.body.appendChild(el);
+      }
+    }
+    + ')();';
+  (document.head || document.documentElement).appendChild(script);
+  script.remove();
+}
+
+function cleanUp() {
+  [vueSelector, knockoutSelector].map(function (selector) {
+    var el = document.getElementById(selector.replace("#", ""));
+    if (el) {
+      el.remove();
+    }
+  });
+}
 
 function checkFE() {
   var frontend = '';
+  injectScript();
   if (document.querySelector(reactSelector) || (window.require && window.require('React'))) {
     frontend = 'react';
   } else if(document.querySelector(angularSelector)) {
     frontend = 'angular';
   } else if(document.querySelector(emberSelector)) {
     frontend = 'ember';
+  } else if(document.querySelector(vueSelector)) {
+    frontend = 'vue';
+  } else if(document.querySelector(knockoutSelector)) {
+    frontend = 'knockout';
   } else {
     frontend = 'none';
   }
   chrome.runtime.sendMessage({ framework: frontend });
+  cleanUp();
 }
 
 chrome.runtime.onMessage.addListener(function() {
